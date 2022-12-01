392dc9dff79f6e3f728081716cbbb07d922c841a
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-12-01 17:12:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -51,8 +51,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-// Function to GeneratePdf
-	function GeneratePdf() {
-		var element = document.getElementById('form-print');
-		html2pdf(element);
-	}
